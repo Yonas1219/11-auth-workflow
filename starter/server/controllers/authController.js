68ef8fffbffbb409af12975c0aae075192f76baa
Yonas Enchalew <yonasenchalew@gmail.com>
2023-06-01 00:45:12 +0300

    changes

--- starter/server/controllers/authController.js
+++ starter/server/controllers/authController.js
@@ -32,8 +32,22 @@ const register = async (req, res) => {
 
 const verifyEmail = async (req, res) => {
   const { verificationToken, email } = req.body;
+const user = await User.findOne({email});
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Verification Failed')
+  }
+  if(user.verificationToken !== verificationToken) {
+    throw new CustomError.UnauthenticatedError('Verification Failed')
+  }
 
-  res.status(StatusCodes.OK).json({verificationToken, email})
+  user.isVerified = true,
+  user.verified = Date.now(),
+  user.verificationToken = ''
+
+  await user.save();
+
+res.status(StatusCodes.OK).json({msg: 'Email Verified'});
+
 }
 
 const login = async (req, res) => {

d341287129eaa691669292c0ed86451e914507d9
Yonas Enchalew <yonasenchalew@gmail.com>
2023-06-03 19:12:56 +0300

    add sendEmail

--- starter/server/utils/sendEmail.js
+++ starter/server/utils/sendEmail.js
@@ -1,13 +1,13 @@
-const nodemailer = require("nodemailer");
-const nodemailerConfig = require("./nodemailerConfig");
+const nodemailer = require('nodemailer');
+const nodemailerConfig = require('./nodemailerConfig');
+
 const sendEmail = async ({ to, subject, html }) => {
   let testAccount = await nodemailer.createTestAccount();
 
-  const transporter = nodemailer.createTransport();
+  const transporter = nodemailer.createTransport(nodemailerConfig);
 
-  // send mail with defined transport object
   return transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    from: '"Coding Addict" <codingaddict@gmail.com>', // sender address
     to,
     subject,
     html,
